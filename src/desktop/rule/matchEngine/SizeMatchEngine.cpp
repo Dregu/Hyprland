18ce13be2a72263ec9cf958da418a5949dea2ee5
Dregu <dregu@dreg.us>
2026-08-27 05:50:59 +0300

    accept whatever

--- src/desktop/rule/matchEngine/SizeMatchEngine.cpp
+++ src/desktop/rule/matchEngine/SizeMatchEngine.cpp
@@ -7,12 +7,13 @@
 using namespace Desktop::Rule;
 
 CSizeMatchEngine::CSizeMatchEngine(const std::string& s) {
-    std::istringstream ss(s);
-    int                x, y;
-    if (ss >> x >> y)
-        m_value = {x, y};
-    else
-        Log::logger->log(Log::ERR, "CSizeMatchEngine: invalid input {}", s);
+    std::string text(s);
+    std::ranges::replace_if(text, [](unsigned char c) { return !std::isdigit(c); }, ' ');
+    std::istringstream ss(text);
+    int                x = -1;
+    int                y = -1;
+    ss >> x >> y;
+    m_value = {x, y};
 }
 
 bool CSizeMatchEngine::match(Hyprutils::Math::Vector2D other) {
